a9f5f1bf8269571243315e24908bbd2246f78ec2
beachboy732 <adamjsimon1@yahoo.com>
2018-07-03 11:24:50 -0400

    added user model

--- models.py
+++ models.py
@@ -7,7 +7,19 @@ class Book(db.Model):
     isbn = db.Column(db.String(20), unique=True, index=True)
     date_published = db.Column(db.DateTime())
     title = db.Column(db.String(255), index=True)
+    date_added = db.Column(db.DateTime())
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.owner_id'))
     # returned list from 'fetchall', matches this order
 
     def __repr__(self):
         return '<Book {} by {}>'.format(self.title, self.author)
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+    owner_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    books = db.relationship('Book', backref='owner', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
